81d8fb1898cba27302d7c86b314bfbc448c8f98a
Constantin Jean <cjean@pc-gi-439.utbm.fr>
2014-12-18 19:00:04 +0100

    restarting project

--- src/com/model/card/DevelopmentCard.java
+++
@@ -1,9 +0,0 @@
-package com.model.card;
-
-public abstract class DevelopmentCard {
-
-	/********************** Attributes **********************/
-		
-	/*********************** Methods ***********************/
-	public void play(){}
-}
--- src/com/observer/Observable.java
+++
@@ -1,7 +0,0 @@
-package com.observer;
-
-public interface Observable {
-	public void addObserver(Observer obs);
-	public void removeObservers();
-	public void UpdateObserver(String msg);
-}
